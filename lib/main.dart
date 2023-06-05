7258eec2330df96ef1443d48d5b8de7c342581da
MosaadIbrahimB <mosaadibrahim85@gmail.com>
2023-06-05 03:05:13 +0200

    finsh quran tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:islam_app/model/theme_model.dart';
 import 'package:islam_app/screen/home_screen_islam_app.dart';
+import 'package:islam_app/screen/quran/sura_details_screen.dart';
 
-void main(){
+void main() {
   runApp(IslamApp());
 }
+
 class IslamApp extends StatelessWidget {
   const IslamApp({Key? key}) : super(key: key);
 
@@ -13,13 +15,12 @@ class IslamApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        HomeScreenIslamApp.routeName:(_)=>HomeScreenIslamApp(),
-      } ,
+        HomeScreenIslamApp.routeName: (_) => HomeScreenIslamApp(),
+        SouraDetails.routeName: (_) => SouraDetails(),
+      },
       initialRoute: HomeScreenIslamApp.routeName,
+      theme: MyTheme.themeLight,
 
-    theme:MyTheme.theme_light ,
     );
   }
 }
-
-
